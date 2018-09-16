9b6fa07f3f364f0cf04f6fa455a8ee130c7581d8
Jack Bruce <jackbruce0@gmail.com>
2018-09-16 12:25:15 -0700

    Added legacy art

---
+++ Assets/scripts/PipeGapController.cs
@@ -0,0 +1,29 @@
+﻿/* PipeGapController.cs
+ * Author: Jack Bruce
+ * Date Created: 8/24
+ * Date Last Edited: 8/15/18
+ * Description: Controls game states, sets pages for 
+ * 
+ * Here's what I'm thinkin
+ * -create a public static jumpAverage variable that will be updated every jump in ScoreManager
+ * -access that jump average in this script (using an OnJump event)
+ * -the higher jumpAverage is the bigger the gap btwn Top Pipe and Btm Pipe
+ * 
+ * 
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeGapController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
